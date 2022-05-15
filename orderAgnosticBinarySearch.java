bb8441b9ca12a7dbb6cea353d7fc1b757568f50f
aishwarya42 <42571918+aishwarya42@users.noreply.github.com>
2022-05-15 19:48:04 +0530

    Add files via upload

--- orderAgnosticBinarySearch.java
+++ orderAgnosticBinarySearch.java
@@ -5,42 +5,40 @@ public class orderAgnosticBinarySearch {
         Scanner sc = new Scanner(System.in);
 
     int[] arr = {90, 88,76,67,43, 34};
-    int tar = 67;
-    int ans = orderagnosticbinary(arr,tar);
+    int tar = 43;
+    int ans = orderAgnosticBS(arr,tar);
         System.out.println("searched element index: "+ ans);
     }
 
-    static int orderagnosticbinary(int[] arr,int tar) {
+    static int orderAgnosticBS(int[] arr, int target) {
         int start = 0;
-        int end = arr.length-1; //to check out of bound size for array
+        int end = arr.length - 1;
 
-        boolean IsAsc = arr[start] < arr[end];
+        // find whether the array is sorted in ascending or descending
+        boolean isAsc = arr[start] < arr[end];
 
-
-        while (start <= end) {
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
             int mid = start + (end - start) / 2;
 
-            if(tar == arr[mid]){
+            if (arr[mid] == target) {
                 return mid;
             }
 
-            if (IsAsc = true) {
-                if (tar < arr[mid]) {
-                    end = mid-1;
+            if (isAsc) {
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
                 }
-                else{
-                    start = mid+1;
+            } else {
+                if (target > arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
                 }
             }
-            else{     //descending order array
-                    if (tar > arr[mid]) {
-                        end = mid-1;
-                    }
-                    else{
-                        start = mid+1;
-                    }
-            }
-
         }
         return -1;
     }
