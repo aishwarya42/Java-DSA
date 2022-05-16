7455ca03edd65434ee307e256de97192a5d941a5
aishwarya42 <42571918+aishwarya42@users.noreply.github.com>
2022-05-16 21:26:34 +0530

    Add files via upload

---
+++ pivotsearchDuplicateElementsInSortedArray.java
@@ -0,0 +1,20 @@
+//package algorithms;
+//import java.util.*;
+//public class pivotsearchDuplicateElementsInSortedArray {
+//    public static void main(String[] args){
+//
+//        int[] arr = {2,2,2,2,9,2,2};
+//
+//    }
+//
+//    static int pivotduplicate(int[] arr){
+//    int start = 0;
+//    int end = arr.length;
+//
+//    while(start<=end){
+//        int mid = start +(end - start)/2;
+//
+//        if()
+//    }
+//
+//}
